fd0f67f106daf5d18a5fbe94a8c0a3af770e1764
Samantha <samanthavillafana8@gmail.com>
2024-05-15 19:09:02 -0400

    complete lab

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,8 +4,23 @@ import "./NavBar.css";
 function NavBar() {
   return (
     <nav className="navbar">
-    
+      <NavLink 
+      to="/"
+      >
+        Home
+      </NavLink>
+      <NavLink 
+      to="/directors"
+      >
+        Directors
+      </NavLink>
+      <NavLink 
+      to="/actors"
+      >
+        Actors
+      </NavLink>
     </nav>
+    
     );
 };
 
--- src/pages/Movie.js
+++ src/pages/Movie.js
@@ -1,13 +1,37 @@
 import { useEffect, useState } from "react";
+import NavBar from "../components/NavBar";
+import { useParams } from "react-router-dom";
 
 function Movie() {
+  const [movie, setMovie] = useState({});
+  const params = useParams();
+  const movieId = params.id;
+
+  useEffect(() => {
+    fetch(`http://localhost:4000/movies/${movieId}`)
+    .then(res => res.json())
+    .then(data => setMovie(data))
+    .catch(error => console.log(error))
+  }, [movieId]);
+
+  const renderGenres = () => {
+    return movie.genres && movie.genres.map((genre, index) => (
+      <span key={index}>{genre}</span>
+    ));
+  };
+
   return (
     <>
       <header>
-        {/* What component should go here? */}
+        <NavBar />
       </header>
       <main>
-        {/* Movie info here! */}
+        <h1>{movie.title}</h1>
+        <p>Length: {movie.time}</p>
+        <span>Genres:</span>
+        <div>
+          {renderGenres()}
+        </div>
       </main>
     </>
   );
